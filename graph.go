ab426c5da2f5ee04bf4502125f2951eca12f354c
Sonia Keys <soniakeys@gmail.com>
2013-11-18 15:37:47 -0500

    note 1.2 requirement

--- graph.go
+++ graph.go
@@ -6,6 +6,8 @@
 // Dijkstra's algorithm is efficient for finding the shortest path between
 // two nodes in a general directed or undirected graph.  The path length
 // minimized is the sum of edge lengths in the path, which must be non-negative.
+//
+// Graph requires Go 1.2.  (It uses the new heap.Fix method.)
 package graph
 
 import "container/heap"
