9b2ddb00f44a0bd2db3afc8531010667824a2a86
Sonia Keys <soniakeys@gmail.com>
2014-01-16 16:37:11 -0500

    don't duplicate example

--- dijkstra_test.go
+++ dijkstra_test.go
@@ -4,80 +4,23 @@
 package graph_test
 
 import (
-	"fmt"
 	"math"
 	"testing"
 
 	"github.com/soniakeys/graph"
 )
 
-// djNode represents a node in a directed graph.  It represents directed edges
-// from the node with the handy DistanceNeighbor type from the graph package.
-type djNode struct {
-	nbs  []graph.DistanceNeighbor // directed edges as DistanceNeighbors
-	name string                   // example application specific data
-}
+// minimal node type with no neighbors.
+type djNode0 struct{}
 
-// djEdge is a simple number representing an edge length/distance/weight.
-type djEdge float64
-
-// djNode implements graph.DistanceNode, also fmt.Stringer
-func (n *djNode) DistanceNeighbors([]graph.DistanceNeighbor) []graph.DistanceNeighbor {
-	return n.nbs
-}
-func (n *djNode) String() string { return n.name }
-
-// djEdge implements graph.DistanceEdge
-func (e djEdge) Distance() float64 { return float64(e) }
-
-var (
-	djNodeData = []string{"a", "b", "c", "d", "e", "f"}
-	djEdgeData = []struct {
-		v1, v2 string
-		l      float64
-	}{
-		{"a", "b", 7},
-		{"a", "c", 9},
-		{"a", "f", 14},
-		{"b", "c", 10},
-		{"b", "d", 15},
-		{"c", "d", 11},
-		{"c", "f", 2},
-		{"d", "e", 6},
-		{"e", "f", 9},
-	}
-)
-
-// linkDjGraph constructs a linked representation of example data.
-func linkDjGraph() (startNode, endNode *djNode) {
-	all := map[string]*djNode{}
-	for _, n := range djNodeData {
-		all[n] = &djNode{name: n}
-	}
-	// link neighbors
-	for _, ge := range djEdgeData {
-		n1 := all[ge.v1]
-		n1.nbs = append(n1.nbs,
-			graph.DistanceNeighbor{djEdge(ge.l), all[ge.v2]})
-	}
-	return all["a"], all["e"]
+func (n *djNode0) DistanceNeighbors([]graph.DistanceNeighbor) []graph.DistanceNeighbor {
+	return nil
 }
 
 func TestDijkstraDirected(t *testing.T) {
-	// construct linked representation of example data
-	startNode, endNode := linkDjGraph()
-	// run Dijkstra's shortest path algorithm
-	p, l := graph.DijkstraShortestPath(startNode, endNode)
-	expected := "[{<nil> a} {9 c} {11 d} {6 e}]"
-	got := fmt.Sprint(p)
-	if got != expected {
-		t.Fatal("wrong path.  expected", expected, "got", got)
-	}
-	if l != 26 {
-		t.Fatal("wrong path length.  expected 26, got", l)
-	}
-	// reverse path should not exist with directed example data
-	p, l = graph.DijkstraShortestPath(endNode, startNode)
+	// search from node with no neighbors to something other than itself
+	// should return no path.
+	p, l := graph.DijkstraShortestPath(&djNode0{}, nil)
 	if p != nil {
 		t.Fatal("wrong path.  expected nil, got", p)
 	}
