76b705b78c19b70fceecf7386d913c1f19e1d573
Sonia Keys <soniakeys@gmail.com>
2014-01-19 01:40:50 -0500

    doc

--- adj/adj.go
+++ adj/adj.go
@@ -1,6 +1,7 @@
 // Copyright 2013 Sonia Keys
 // License MIT: http://opensource.org/licenses/MIT
 
+// Adj defines concrete types and methods for an adjacency graph.
 package adj
 
 import (
@@ -9,35 +10,53 @@ import (
 	"github.com/soniakeys/graph"
 )
 
-// The example contains an example graph representation and example data.
-// The representation implements graph.EstimateNode and graph.DistanceEdge.
-
-// estNode represents a node that can return a heuristic distance estimate
-// to another node.  It represents directed edges from the node with the handy
-// EstimateNeighbor type from the graph package.
+// Node represents a node in an adjacency graph.  It implements
+// graph.EstimateNode and fmt.Stringer.
 type Node struct {
 	Data interface{}      // application specific
 	Nbs  []graph.Neighbor // directed edges
 }
 
-// Node implements graph.EstimateNode, also fmt.Stringer
+// Visit visits neighbors of a node.
 func (n *Node) Visit(v graph.NeighborVisitor) {
 	for _, nb := range n.Nbs {
 		v(nb)
 	}
 }
+
+// Estimate obtains a heuristic distance estimate through the Estimator
+// interface of the Data field of the receiver.  This panics if n.Data
+// does not impliment Estimator.  It does not default to a null heuristic.
+// Data is not required to implement Estimator, but if it does not implement
+// Estimator, it is a programming error to use it in a context that requests
+// estimates.
 func (n *Node) Estimate(e graph.EstimateNode) float64 {
 	return n.Data.(graph.Estimator).Estimate(e)
 }
+
+// String returns a printed representation of n.Data.
 func (n *Node) String() string { return fmt.Sprint(n.Data) }
 
-// Edge implements graph.DistanceEdge
+// Edge implements graph.DistanceEdge.
 type Edge float64
 
+// Distance returns edge distance.
 func (e Edge) Distance() float64 { return float64(e) }
 
+// Graph defines a simple representation for a set of Nodes.
 type Graph map[interface{}]*Node
 
+// Link sets one Node of a Graph to be a neighbor of another, adding either
+// or both nodes to the graph as neccessary.
+//
+// N1 and n2 are used as node keys and are also assigned to the Data fields
+// when nodes are first added to the graph.  Because n1 and n2 are used as
+// map keys, their concrete types must be Go-comparable.  N1 and n2 may be
+// of any comparable type but if they will be used in a context that uses
+// Nodes as graph.EstimateNodes for example, they must implement
+// graph.Estimator.  Similarly, ed may be of any type, but it will be used
+// as a graph.Edge.  If the Graph g will be used in a context that uses edges
+// as graph.DistanceEdges, ed must implement graph.DistanceEdge.
 func (g Graph) Link(n1, n2, ed interface{}) {
 	nd2, ok := g[n2]
 	if !ok {
