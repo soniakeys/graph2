ae397f7aef7f1b4d1c4b5b2f08e8493ec73769d5
Sonia Keys <soniakeys@gmail.com>
2014-02-05 23:15:55 -0500

    separate adj and defined type examples

---
+++ adj/astarm_ex_test.go
@@ -0,0 +1,57 @@
+// Copyright 2013 Sonia Keys
+// License MIT: http://opensource.org/licenses/MIT
+
+package adj_test
+
+import (
+	"fmt"
+
+	"github.com/soniakeys/graph"
+	"github.com/soniakeys/graph/adj"
+	"github.com/soniakeys/graph/search"
+)
+
+// Adj types satisfy the interfaces required by AStarM.  The only additional
+// requirement is a node data type that implements graph.Estimator.
+
+// amData represents node data.
+type amData struct {
+	name string  // example application specific data
+	h    float64 // heuristic distance estimate to end node
+}
+
+// Implement graph.Estimator as required.
+func (n *amData) Estimate(graph.EstimateNode) float64 { return n.h }
+
+// Implement fmt.String for nice output.
+func (n *amData) String() string { return n.name }
+
+func ExampleAStarM() {
+	a := &amData{"a", 19}
+	b := &amData{"b", 20}
+	c := &amData{"c", 10}
+	d := &amData{"d", 6}
+	e := &amData{"e", 0}
+	f := &amData{"f", 9}
+	// construct graph using adj types
+	g := adj.Digraph{}
+	g.Link(a, b, adj.Weighted(7))
+	g.Link(a, c, adj.Weighted(9))
+	g.Link(a, f, adj.Weighted(14))
+	g.Link(b, c, adj.Weighted(10))
+	g.Link(b, d, adj.Weighted(15))
+	g.Link(c, d, adj.Weighted(11))
+	g.Link(c, f, adj.Weighted(2))
+	g.Link(d, e, adj.Weighted(6))
+	g.Link(e, f, adj.Weighted(9))
+	// echo initial conditions
+	fmt.Println("Directed graph with", len(g), "nodes")
+	// run AStarM
+	p, l := search.AStarM(g[a], g[e])
+	fmt.Println("Shortest path:", p)
+	fmt.Println("Path length:", l)
+	// Output:
+	// Directed graph with 6 nodes
+	// Shortest path: [{<nil> a} {9 c} {11 d} {6 e}]
+	// Path length: 26
+}
--- search/astarm_ex_test.go
+++ search/astarm_ex_test.go
@@ -7,46 +7,64 @@ import (
 	"fmt"
 
 	"github.com/soniakeys/graph"
-	"github.com/soniakeys/graph/adj"
 	"github.com/soniakeys/graph/search"
 )
 
-// amData represents data for a graph.EstimateNode.
-type amData struct {
-	name string  // example application specific data
-	hEnd float64 // heuristic distance estimate to end node
+// AStarM requires a node type that implements graph.EstimateNode and an
+// edge type that implements graph.Weighted.  Our two types:
+type (
+	monoNode struct {
+		name string      // node name
+		h    float64     // heuristic distance estimate to end node
+		nbs  []graph.Adj // "neighbors," adjacent arcs and nodes
+	}
+	monoArc float64
+)
+
+// Two methods implement graph.Estimator.
+func (n *monoNode) Visit(v graph.AdjVisitor) {
+	for _, a := range n.nbs {
+		v(a)
+	}
+}
+func (n *monoNode) Estimate(graph.EstimateNode) float64 { return n.h }
+
+// One method implements graph.Weighted.
+func (a monoArc) Weight() float64 {
+	return float64(a)
 }
 
-// Implement graph.Estimator and fmt.String
-func (n *amData) Estimate(graph.EstimateNode) float64 { return n.hEnd }
-func (n *amData) String() string                      { return n.name }
+// Implement fmt.Stringer to make output easy.
+func (n *monoNode) String() string { return n.name }
+
+// One more method to make graph construction easy.
+func (n *monoNode) link(n2 *monoNode, weight int) {
+	n.nbs = append(n.nbs, graph.Adj{monoArc(weight), n2})
+}
 
 func ExampleAStarM() {
-	a := &amData{"a", 19}
-	b := &amData{"b", 20}
-	c := &amData{"c", 10}
-	d := &amData{"d", 6}
-	e := &amData{"e", 0}
-	f := &amData{"f", 9}
-	// use package graph/adj
-	g := adj.Digraph{}
-	g.Link(a, b, adj.Weighted(7))
-	g.Link(a, c, adj.Weighted(9))
-	g.Link(a, f, adj.Weighted(14))
-	g.Link(b, c, adj.Weighted(10))
-	g.Link(b, d, adj.Weighted(15))
-	g.Link(c, d, adj.Weighted(11))
-	g.Link(c, f, adj.Weighted(2))
-	g.Link(d, e, adj.Weighted(6))
-	g.Link(e, f, adj.Weighted(9))
-	// echo initial conditions
-	fmt.Println("Directed graph with", len(g), "nodes")
-	// run AStarA
-	p, l := search.AStarM(g[a], g[e])
+	a := &monoNode{name: "a", h: 19}
+	b := &monoNode{name: "b", h: 20}
+	c := &monoNode{name: "c", h: 10}
+	d := &monoNode{name: "d", h: 6}
+	e := &monoNode{name: "e", h: 0}
+	f := &monoNode{name: "f", h: 9}
+	a.link(b, 7)
+	a.link(c, 9)
+	a.link(f, 14)
+	b.link(c, 10)
+	b.link(d, 15)
+	c.link(d, 11)
+	c.link(f, 2)
+	d.link(e, 6)
+	e.link(f, 9)
+	fmt.Println("Directed graph with 6 nodes, 9 edges")
+
+	p, l := search.AStarM(a, e)
 	fmt.Println("Shortest path:", p)
 	fmt.Println("Path length:", l)
 	// Output:
-	// Directed graph with 6 nodes
+	// Directed graph with 6 nodes, 9 edges
 	// Shortest path: [{<nil> a} {9 c} {11 d} {6 e}]
 	// Path length: 26
 }
