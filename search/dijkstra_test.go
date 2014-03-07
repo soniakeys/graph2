cceedd24cd9c3a9dad48bcb33caf3b1378c38007
Sonia Keys <soniakeys@gmail.com>
2014-03-07 15:34:29 -0500

    upgrade tests to match ed package

--- search/dijkstra_test.go
+++ search/dijkstra_test.go
@@ -58,9 +58,10 @@ func (l xyList) Len() int           { return len(l) }
 func (l xyList) Less(i, j int) bool { return l[i].name < l[j].name }
 func (l xyList) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
 
-// generate a random graph
-func r(nNodes, nArcs int) (start, end *stNode) {
-	s := rand.New(rand.NewSource(59))
+var s = rand.New(rand.NewSource(59))
+
+// generate a random directed graph and end points to test
+func r(nNodes, nArcs int, seed int64) (start, end *stNode) {
 	// generate unique node names
 	nameMap := map[string]bool{}
 	name := make([]byte, int(math.Log(float64(nNodes))/3)+1)
@@ -78,34 +79,57 @@ func r(nNodes, nArcs int) (start, end *stNode) {
 		i++
 	}
 	sort.Sort(nodes)
-	// now assign random coordinates.
+	// now generate random coordinates from repeatability seed
+	s.Seed(seed)
 	for i := range nodes {
 		nodes[i].x = s.Float64()
 		nodes[i].y = s.Float64()
 	}
-	// generate edges.
+	// random start
+    start = &nodes[s.Intn(nNodes)]
+    // end is point at distance nearest target distance
+    const target = .3
+    nearest := 2.
+    for i, n2 := range nodes {
+        d := math.Abs(target - math.Hypot(n2.x-start.x, n2.y-start.y))
+        if d < nearest {
+            end = &nodes[i]
+            nearest = d
+        }
+    }
+generateArcs:
 	for i := 0; i < nArcs; {
-		n1 := &nodes[s.Intn(nNodes)]
-		n2 := &nodes[s.Intn(nNodes)]
-		dist := math.Hypot(n2.x-n1.x, n2.y-n1.y)
-		if dist > s.Float64()*math.Sqrt2 {
-			continue
+		n1 := s.Intn(nNodes)
+		n2 := s.Intn(nNodes)
+		nd1 := &nodes[n1]
+		nd2 := &nodes[n2]
+		dist := math.Hypot(nd2.x-nd1.x, nd2.y-nd1.y)
+		if dist > s.ExpFloat64() {
+			continue // favor near nodes
 		}
-		n1.nbs = append(n1.nbs, stArc{dist, n2})
-		switch i {
-		case 0:
-			start = n1
-		case 1:
-			end = n2
+		if n1 == n2 {
+			continue // no self loops
 		}
+		for _, nb := range nd1.nbs {
+			if nb.to == nd2 {
+				continue generateArcs // no parallel arcs
+			}
+		}
+		nd1.nbs = append(nd1.nbs, stArc{dist, nd2})
 		i++
 	}
-	return start, end
+	return
+}
+
+func Test100(t *testing.T) {
+    start, end := r(100, 200, 62)
+    t.Log(search.DijkstraShortestPath(start, end))
+    t.Log(search.DijkstraShortestPath(start, end))
 }
 
 func Benchmark100(b *testing.B) {
 	// 100 nodes
-	start, end := r(100, 200)
+	start, end := r(100, 200, 62)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		search.DijkstraShortestPath(start, end)
@@ -114,7 +138,7 @@ func Benchmark100(b *testing.B) {
 
 func Benchmark1e3(b *testing.B) {
 	// 1000 nodes
-	start, end := r(1000, 3000)
+	start, end := r(1000, 3000, 66)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		search.DijkstraShortestPath(start, end)
@@ -123,7 +147,7 @@ func Benchmark1e3(b *testing.B) {
 
 func Benchmark1e4(b *testing.B) {
 	// 10k nodes
-	start, end := r(1e4, 5e4)
+	start, end := r(1e4, 5e4, 59)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		search.DijkstraShortestPath(start, end)
@@ -132,7 +156,7 @@ func Benchmark1e4(b *testing.B) {
 
 func Benchmark1e5(b *testing.B) {
 	// 100k nodes
-	start, end := r(1e5, 1e6)
+	start, end := r(1e5, 1e6, 59)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		search.DijkstraShortestPath(start, end)
